440330fbabbd5d3a3b83e8add94685b14063ab4f
Kemo8965 <78011284+Kemo8965@users.noreply.github.com>
2021-06-20 23:22:06 +0200

    first commit

---
+++ components/navbar.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+           <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+       <v-toolbar-title v-text="title" />
+        <v-spacer />
+      <v-btn icon @click.stop="fixed = !fixed">
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+     
+    
+      
+    </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+        {
+          icon: 'mdi-user',
+          title: 'Logout',
+          to: '/auth/login',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'eLearner',
+    }
+  },
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/auth/login.vue
@@ -0,0 +1,122 @@
+<template>
+<v-card width="400" class="mx-auto mt-10">
+   <v-card-title>
+       Login with <br> <span class="learn">eLearner</span>
+     </v-card-title>
+
+     <v-card-text>
+         <form>
+     <v-text-field
+      v-model="email"
+      :error-messages="emailErrors"
+      label="E-mail"
+      required
+      prepend-icon="mdi-account-circle"
+      @input="$v.email.$touch()"
+      @blur="$v.email.$touch()"
+    ></v-text-field>
+
+     <v-text-field
+      v-model="password"
+      :error-messages="passwordErrors"
+      
+      label="Password"
+      type= password
+       prepend-icon="mdi-lock"
+       append-icon="mdi-eye-off"
+      required
+      @input="$v.password.$touch()"
+      @blur="$v.password.$touch()"
+    ></v-text-field>
+   
+    <v-card-actions>
+        <v-btn
+      class="mr-4 success"
+      @click="submit"
+    >
+      Register
+    </v-btn>
+
+    <v-btn
+     class="mr-4 info"
+     @click="clear">
+      Login
+    </v-btn>
+    </v-card-actions> 
+    
+  </form>
+ 
+     </v-card-text>
+   </v-card>
+</template>
+
+<<script>
+  import { validationMixin } from 'vuelidate'
+  import { required, maxLength, email } from 'vuelidate/lib/validators'
+
+  export default {
+    mixins: [validationMixin],
+
+    validations: {
+      email: { required, email },
+      password: { required, minLength: maxLength(6) },
+      
+      },
+    
+
+    data: () => ({
+      email: '',
+      password:'',
+      
+    }),
+
+    computed: {
+
+      checkboxErrors () {
+        const errors = []
+        if (!this.$v.checkbox.$dirty) return errors
+        !this.$v.checkbox.checked && errors.push('You must agree to continue!')
+        return errors
+      },
+      selectErrors () {
+        const errors = []
+        if (!this.$v.select.$dirty) return errors
+        !this.$v.select.required && errors.push('Item is required')
+        return errors
+      },
+      passwordErrors () {
+        const errors = []
+        if (!this.$v.password.$dirty) return errors
+        !this.$v.password.maxLength && errors.push('Password must be at least 6 characters long')
+        !this.$v.password.required && errors.push('Password is required.')
+        return errors
+      },
+      emailErrors () {
+        const errors = []
+        if (!this.$v.email.$dirty) return errors
+        !this.$v.email.email && errors.push('Must be valid e-mail')
+        !this.$v.email.required && errors.push('E-mail is required')
+        return errors
+      },
+    },
+
+    methods: {
+      submit () {
+        this.$v.$touch()
+        this.$router.push('/');
+      },
+      clear () {
+        this.$v.$reset() 
+        this.email = ''
+        this.password = ''
+        
+      },
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+.learn{
+  color:blue;
+}
+  </style>
